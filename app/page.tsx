7ae0fcce7e67b75a6c11a445e88e75bf6a702b11
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-12-05 04:45:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+import HeroComponent from '@/components/heroComponent'
+import NewsCard from '@/components/newsCard'
+import NewsHeader from '@/components/newsHeader'
+import LenisProvider from '@/providers/LenisProvider'
+import HeroImg from '@/public/img/Hero.jpg'
+import NewsImg from '@/public/img/newsImg.jpg'
+import Footer from './footer'
+const content = {
+  hero: {
+    heading: 'Change in Band Lineup',
+    date: '2024.02.26',
+    announcement:
+      'With deep sadness, we announce that our talented drummer, Kir, has left our band due to relocating to another country. This is a tough moment for us as Kir was not only a professional but also a good friend.',
+    buttonLabel: 'Read On',
+  },
+  newsSection: {
+    sectionName: "What's going on",
+    title: "News",
+    cards: [
+      {
+        date: "2024.02.26",
+        title: "Change in Band Lineup",
+        content: "With deep sadness, we announce that our talented drummer, Kir, has left our band due to relocating to another country. This is a tough moment for us as Kir was not only a professional but also a good friend.",
+        imageUrl: HeroImg.src
+      },
+      {
+        date: "2024.02.26",
+        title: "Recording New Tracks in Warsaw!",
+        content: "Hey! We recently visited a studio in Warsaw, crafting some fresh tunes. It takes a bit of time, but we're putting our hearts into it to bring you something special.",
+        imageUrl: NewsImg.src
+      }
+    ]
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  return (
+    <LenisProvider>
+      <HeroComponent
+        heading={content.hero.heading}
+        date={content.hero.date}
+        announcement={content.hero.announcement}
+        buttonLabel={content.hero.buttonLabel}
+      />
+      <NewsHeader
+        sectionName={content.newsSection.sectionName}
+        title={content.newsSection.title}
+      />
+      <div className='flex px-4 gap-3'>
+        {content.newsSection.cards.map((card) => (
+          <NewsCard
+            key={card.title}
+            date={card.date}
+            title={card.title}
+            content={card.content}
+            imageUrl={card.imageUrl}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        ))}
+      </div>
+      <span className='text-[24vw] font-black px-6'>HANNA</span>
+      <div
+        className='relative h-[500px]'
+        style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+      >
+        <Footer />
+      </div>
+    </LenisProvider>
+  )
 }
