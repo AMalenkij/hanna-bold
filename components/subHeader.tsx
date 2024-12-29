6948a8d3a8a3b62cb18113a05679bd399818d134
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-12-29 07:17:25 +0100

    Create subHeader.tsx

---
+++ components/subHeader.tsx
@@ -0,0 +1,52 @@
+import ArrowIcon from "@/public/svg/arrowIcon";
+import TriangleIcon from "@/public/svg/triangleIcon";
+import type { FC } from "react";
+
+interface SubHeaderProps {
+  sectionName?: string;
+  title: string;
+  variant?: string;
+  counter?: number;
+}
+
+const SubHeader: FC<SubHeaderProps> = ({
+  sectionName,
+  title,
+  counter,
+  variant,
+}) => {
+  if (variant === "withСounter") {
+    return (
+      <section
+        aria-labelledby={title}
+        className="mx-auto flex w-full items-center justify-between px-4 py-6 text-white"
+      >
+        <div className="space-y-1">
+          <div className="flex gap-3">
+            <h1 className="font-semibold text-5xl uppercase tracking-tight">
+              {title}
+            </h1>
+            <div className="flex flex-col items-center justify-between">
+              <p className="text-center font-semibold text-lg">[ {counter} ]</p>
+              <TriangleIcon />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  return (
+    <section
+      aria-labelledby={title}
+      className="mx-auto flex w-full items-center justify-between px-4 py-6 text-white"
+    >
+      <div className="space-y-1">
+        <p className="text-sm">/ {sectionName}</p>
+        <h1 className="font-bold text-6xl tracking-tight">{title}</h1>
+      </div>
+      <ArrowIcon className="h-24 w-24 rotate-90" />
+    </section>
+  );
+};
+
+export default SubHeader;
