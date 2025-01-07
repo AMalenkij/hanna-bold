e3e2880e48956a165a669bb46de2272fa5d8589a
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2025-01-07 19:18:30 +0100

    Update navLink.tsx

--- components/navLink.tsx
+++ components/navLink.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button"
-import { Link } from "@/i18n/routing"
-import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+import { Link } from "@/i18n/routing";
 
 interface NavLinkProps {
-  href: string
-  label: string
-  isActive: boolean
-  className?: string
+  href: string;
+  label: string;
+  isActive: boolean;
+  className?: string;
 }
 
 export default function NavLink({
@@ -15,23 +14,17 @@ export default function NavLink({
   isActive,
   className,
 }: NavLinkProps) {
-  const linkClasses = cn(
-    "transition-colors hover:text-foreground/80 text-lg",
-    isActive ? "text-foreground pointer-events-none" : "text-foreground/60",
-    className,
-  )
-
   if (isActive) {
     return (
-      <Button variant="link" size="sm" className={linkClasses}>
+      <Button variant="link" size="sm" className={className} disabled>
         {label}
       </Button>
-    )
+    );
   }
 
   return (
-    <Button variant="link" size="sm" className={linkClasses}>
+    <Button variant="link" size="sm" className={className}>
       <Link href={href}>{label}</Link>
     </Button>
-  )
+  );
 }
