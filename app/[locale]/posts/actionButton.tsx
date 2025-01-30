0c6442d01ba52d5fe37537b05b3ae3688d4ba3d5
Anton Malenkij <itottoro@gmail.com>
2025-01-30 23:14:20 +0100

    improving the code structure

---
+++ app/[locale]/posts/actionButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import type { ActionType } from "@/types/common";
+import type { ReactNode } from "react";
+
+interface BaseActionProps {
+  actionType: ActionType;
+  buttonLabel: string;
+  icon: ReactNode;
+  children: ReactNode;
+}
+
+export function ActionButton({
+  actionType,
+  buttonLabel,
+  icon,
+  children,
+}: BaseActionProps) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" data-action-type={actionType}>
+          {icon}
+          {buttonLabel}
+        </Button>
+      </DialogTrigger>
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
+  );
+}
