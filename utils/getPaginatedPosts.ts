2702492e6530dad04d7ba863075585f60d022466
Anton Malenkij <itottoro@gmail.com>
2025-01-31 00:52:08 +0100

    Update getPaginatedPosts.ts

--- utils/getPaginatedPosts.ts
+++ utils/getPaginatedPosts.ts
@@ -1,53 +1,54 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import type { Locale } from "@/types/common";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default async function getPaginatedPosts(params: {
-	page?: number
-	locale: "en" | "uk" | "pl"
-	published?: boolean
+  page?: number;
+  locale: Locale;
+  published?: boolean;
 }) {
-	const { page = 1, locale = "en", published = true } = params
+  const { page = 1, locale = "en", published = true } = params;
 
-	const pageSize = 4
-	const skip = (page - 1) * pageSize
+  const pageSize = 4;
+  const skip = (page - 1) * pageSize;
 
-	// Получаем посты
-	const posts = await prisma.posts.findMany({
-		where: {
-			is_published: published,
-			// Используем корректное условие для проверки непустого поля
-			[`title_${locale}`]: { not: undefined },
-		},
-		select: {
-			id: true,
-			[`title_${locale}`]: true,
-			[`intro_${locale}`]: true,
-			[`content_${locale}`]: true,
-			slug: true,
-			photo: true,
-			created_at: true,
-		},
-		orderBy: { created_at: "desc" },
-		take: pageSize,
-		skip: skip,
-	})
+  // Получаем посты
+  const posts = await prisma.posts.findMany({
+    where: {
+      is_published: published,
+      // Используем корректное условие для проверки непустого поля
+      // [`title_${locale}`]: { not: undefined },
+    },
+    // select: {
+    //   id: true,
+    //   [`title_${locale}`]: true,
+    //   [`intro_${locale}`]: true,
+    //   [`content_${locale}`]: true,
+    //   slug: true,
+    //   photo: true,
+    //   created_at: true,
+    // },
+    orderBy: { created_at: "desc" },
+    take: pageSize,
+    skip: skip,
+  });
 
-	// Получаем общее количество постов
-	const totalPosts = await prisma.posts.count({
-		where: {
-			is_published: published,
-			[`title_${locale}`]: { not: undefined },
-		},
-	})
+  // Получаем общее количество постов
+  const totalPosts = await prisma.posts.count({
+    where: {
+      is_published: published,
+      // [`title_${locale}`]: { not: undefined },
+    },
+  });
 
-	return {
-		posts,
-		pagination: {
-			currentPage: page,
-			pageSize,
-			totalPosts,
-			totalPages: Math.ceil(totalPosts / pageSize),
-		},
-	}
+  return {
+    posts,
+    pagination: {
+      currentPage: page,
+      pageSize,
+      totalPosts,
+      totalPages: Math.ceil(totalPosts / pageSize),
+    },
+  };
 }
