cafc05bdfad363e66dd67daade5e52a98cb20ff7
Anton Malenkij <itottoro@gmail.com>
2025-02-17 20:53:47 +0100

    Update createPostAction.ts

--- actions/createPostAction.ts
+++ actions/createPostAction.ts
@@ -1,33 +1,36 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
+import { uploadImageToCloudinary } from "./uploadImageToCloudinary";
 
 const prisma = new PrismaClient();
 
 export async function createPostAction(formData: FormData) {
   try {
+    const file = formData.get("photo") as File;
+    const slug = formData.get("slug") as string;
+
+    let photoUrl = null;
+    if (file.size > 0) {
+      photoUrl = await uploadImageToCloudinary(file, slug);
+    }
+
     const post = await prisma.posts.create({
       data: {
-        // Multilingual titles
         title_en: formData.get("title_en") as string,
         title_ua: formData.get("title_ua") as string,
         title_pl: formData.get("title_pl") as string,
 
-        // Multilingual intros
         intro_en: formData.get("intro_en") as string,
         intro_ua: formData.get("intro_ua") as string,
         intro_pl: formData.get("intro_pl") as string,
 
-        // Multilingual content
         content_en: formData.get("content_en") as string,
         content_ua: formData.get("content_ua") as string,
         content_pl: formData.get("content_pl") as string,
 
-        // Slug
-        slug: formData.get("slug") as string,
-
-        // Optional fields
-        photo: (formData.get("photo") as string) || null,
+        slug,
+        photo: photoUrl,
         is_published: formData.get("is_published") === "true",
       },
     });
