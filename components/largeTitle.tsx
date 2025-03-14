db1e42b8ad46c31a3d8e165f00fe73a68f3750f5
Anton Malenkij <itottoro@gmail.com>
2025-03-14 14:37:09 +0100

    add band description

--- components/largeTitle.tsx
+++ components/largeTitle.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
-import { BAND_NAME, BAND_SHORT_NAME } from "@/constants/setting";
+import {
+  BAND_NAME,
+  BAND_SHORT_NAME,
+  BAND_DESCRIPTION,
+} from "@/constants/setting";
 
 type LargeTitleProps = {
   className?: string;
@@ -7,13 +11,30 @@ type LargeTitleProps = {
 
 export default function LargeTitle({ className }: LargeTitleProps) {
   return (
-    <div className={cn("flex justify-center font-black", className)}>
-      <span className="hidden text-10xl md:block lg:text-13xl xl:text-14xl 2xl:text-15xl">
-        {BAND_NAME}
-      </span>
-      <span className="block text-13xl sm:text-14xl md:hidden">
-        {BAND_SHORT_NAME}
-      </span>
+    <div className={cn("flex flex-col items-center pb-6 2xl:pb-8", className)}>
+      {/* Desctop */}
+      <div className="group relative hidden md:block">
+        <div className="relative inline-block text-center">
+          <p className="font-black text-10xl leading-none lg:text-13xl xl:text-14xl 2xl:text-15xl">
+            {BAND_NAME}
+          </p>
+          <span className="2xl:-right-1.5 lg:-right-1 -tracking-[.05em] absolute right-0 bottom-6 translate-y-full whitespace-nowrap font-light text-3xl italic lg:bottom-8 lg:text-4xl lg:tracking-[.09em] xl:right-0.5 xl:bottom-10 xl:text-5xl xl:tracking-[.02em] 2xl:bottom-14 2xl:text-7xl 2xl:tracking-[.03em]">
+            {BAND_DESCRIPTION}
+          </span>
+        </div>
+      </div>
+
+      {/* Mobile */}
+      <div className="group relative block md:hidden">
+        <div className="relative inline-block text-center">
+          <p className="font-black text-13xl leading-none sm:text-14xl">
+            {BAND_SHORT_NAME}
+          </p>
+          <span className="absolute right-4 bottom-8 translate-y-full whitespace-nowrap font-light text-3xl italic tracking-wider sm:right-5 sm:text-4xl">
+            {BAND_DESCRIPTION}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- constants/setting.ts
+++ constants/setting.ts
@@ -1,3 +1,4 @@
 export const BAND_NAME = "HANNA";
 export const BAND_SHORT_NAME = "HN";
 export const CONTACT_EMAIL = "hannabandgd@gmail.com";
+export const BAND_DESCRIPTION = "rock-band";
