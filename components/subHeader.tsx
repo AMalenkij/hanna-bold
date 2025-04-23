d9d61b65d96b869b80da6c951a0211cba8c864b5
Anton Malenkij <itottoro@gmail.com>
2025-04-24 01:09:20 +0200

    fix: small style change

--- components/subHeader.tsx
+++ components/subHeader.tsx
@@ -20,7 +20,7 @@ const SubHeader: FC<SubHeaderProps> = ({
     return (
       <section
         aria-labelledby={title}
-        className="flex w-full justify-between py-6 text-foreground"
+        className="flex w-full justify-between py-12 text-foreground"
       >
         <div className="w-full space-y-1 ">
           {variant === "withСounterNotIcon" ? (
@@ -57,7 +57,7 @@ const SubHeader: FC<SubHeaderProps> = ({
   }
   return (
     // POSTS
-    <section aria-labelledby={title} className="-ml-1.5 py-6 text-foreground">
+    <section aria-labelledby={title} className="-ml-1.5 py-12 text-foreground">
       <div className="space-y-1">
         <p className="text-sm md:ml-3.5 lg:ml-3.5 ">/ {sectionName}</p>
         <div className="flex items-center justify-between">
